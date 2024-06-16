2cbf246846e7655ee441412f713f00578e6bad6f
patrick hermann <47567770+patrick-hermann-sva@users.noreply.github.com>
2024-06-16 14:18:45 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,14 +14,10 @@ provider "flux" {
 }
 
 resource "flux_bootstrap_git" "flux2" {
-  depends_on = [kubernetes_manifest.additional_manifests]
+  depends_on = [kubernetes_manifest.additional_manifests, github_repository_deploy_key.ssh]
 
   path       = var.target_path
-  depends_on = [github_repository_deploy_key.ssh]
-  components_extra = [
-    "image-reflector-controller",
-    "image-automation-controller"
-  ]
+  components_extra = var.components_extra
   kustomization_override = templatefile("${path.module}/templates/kustomization-patch.yaml.tpl", { patch = var.kustomization_patch })
   embedded_manifests     = true
 }
